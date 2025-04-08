186d95884e023dd866379f0e6d81b47e86281ef7
João Lima <joaovictorbernardinodelima@gmail.com>
2025-04-08 09:59:18 -0300

    adiciona exemplos de códigos

--- comment.py
+++ comment.py
@@ -1,2 +1,3 @@
 #apenas trabalhando com comentarios
 print("Ola, você não pode ver mas esse código tem comentários")
+print("Essa font ta legal to gostando dela mas nao chega perto nao das outras fontes, aparentemente não tem fontligatures.")
---
+++ lists/cars.py
@@ -0,0 +1,8 @@
+cars = ["bmw", "toyota", "audi", "mercedez", "subaru"]
+print(cars)
+
+print("\nSorted list: ")
+print(sorted(cars))
+
+print("\nOriinal list: ")
+print(cars)
